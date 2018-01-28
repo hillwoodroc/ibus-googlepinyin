65d42cdaac1f6d16d4e97fedee6bdcfb781ab0af
hillwoodroc <hillwoodroc@gmail.com>
2018-01-28 14:36:55 +0800

    Fix path for dict on 64bit.

--- googlepinyin.py
+++ googlepinyin.py
@@ -17,7 +17,9 @@ ime_pinyin = ctypes.CDLL(ctypes.util.find_library('googlepinyin'), use_errno=Tru
 _a = lambda path: os.path.exists(path) and path or ""
 FN_SYS_DICT = _a('/usr/share/googlepinyin/dict_pinyin.dat')\
            or _a('/usr/lib/googlepinyin/data/dict_pinyin.dat')\
+           or _a('/usr/lib64/googlepinyin/data/dict_pinyin.dat')\
            or (glob.glob('/usr/lib/*/googlepinyin/data/dict_pinyin.dat') + [''])[0]\
+           or (glob.glob('/usr/lib64/*/googlepinyin/data/dict_pinyin.dat') + [''])[0]\
            or _a('/usr/local/share/googlepinyin/dict_pinyin.dat')
 FN_USR_DICT = os.path.expanduser('~/.config/ibus/googlepinyin/userdict_pinyin.dat')
 
